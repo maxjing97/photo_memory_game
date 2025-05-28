bbdc1c61604446b2e7b1d0d54799b8f8136ee21c
maxjing <maxjing97@gmail.com>
2025-05-28 19:07:21 +0800

    basic nav bar routing template

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route,Routes } from 'react-router-dom';
+import Navbar from './pages/navbar';
+import Sample from './pages/test';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <>
+    <Navbar/>
+      <div>
+    <Routes>        
+      <Route path="/"  element={<Sample/>} />
+      <Route path="/favourites"  element={<Sample />}/>
+      <Route path="/cart"  element={<Sample/>}/>
+    </Routes>
     </div>
+    </>
+   
   );
 }
 
---
+++ src/pages/test.js
@@ -0,0 +1,12 @@
+
+
+function Sample() {
+  return (
+   <h1>
+        test
+    </h1>
+   
+  );
+}
+
+export default Sample;
