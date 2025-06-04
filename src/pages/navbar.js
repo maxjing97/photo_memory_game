86949edc19e969be33b0360260d36194c5ba7647
maxjing <maxjing97@gmail.com>
2025-06-04 21:52:21 +0800

    added a logo, and centered all elements of the navbar

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -6,6 +6,8 @@ import "../App.css"
 const Navbar=()=>{
     return (    
         <div className='navBar'>
+            <img className="logo" src="logo.png"/>
+            <h1 className="title">Word-Image Memory Tester</h1>
             <div className='main'>
                 <Link to="/">Test</Link>
             </div>
